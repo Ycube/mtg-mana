1701d65240ea4396369e3e80aaf670b8b034786a
Yale Yuen <yaleyyuen@gmail.com>
2016-05-26 09:08:49 -0700

    add ui-route

--- client/app.js
+++ client/app.js
@@ -1,1 +1,8 @@
-angular.module('mtgMana', []);
+var myApp = angular.module('mtgMana', ['ui.router']);
+myApp.config(($stateProvider, $urlRouterProvider) => {
+  $stateProvider
+    .state('home', {
+      url:'/home',
+      templateurl: 'index.html'
+    })
+});
