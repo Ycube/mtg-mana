1b239d3427d6871f0236da1e27438b1211c04ba9
Yale Yuen <yaleyyuen@gmail.com>
2016-05-26 11:22:06 -0700

    implement form for mana

--- client/app.js
+++ client/app.js
@@ -1,8 +1,34 @@
 var myApp = angular.module('mtgMana', ['ui.router']);
+
 myApp.config(($stateProvider, $urlRouterProvider) => {
+
+  $urlRouterProvider.otherwise('/');
+
   $stateProvider
     .state('home', {
       url:'/home',
       templateurl: 'index.html'
     })
 });
+
+myApp.controller('manaController',[ '$scope', ($scope) => {
+  
+  $scope.manaType = [
+    {'color': 'white', 'value': 0}, 
+    {'color': 'blue', 'value': 0}, 
+    {'color': 'black', 'value': 0}, 
+    {'color': 'red', 'value': 0}, 
+    {'color': 'green', 'value': 0}];
+  
+  $scope.update = (index) => {
+
+  };
+
+  // $scope.removeMana = (index) => {
+  //   if($scope.manaType.value > 0) {
+  //     $scope.manaType.value--;
+  //   }
+  // }
+
+}]);
+
